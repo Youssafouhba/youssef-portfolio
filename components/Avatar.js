5d46da6143593a006831ab585c047d82daf917f4
Youssafouhba <146470979+Youssafouhba@users.noreply.github.com>
2023-12-01 19:13:32 +0100

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -6,7 +6,7 @@ const Avatar = () => {
   return (
   <div className='hidden xl:flex xl:max-w-none'>
     <Image 
-    src={'/avatar.png'}
+    src={'./build/avatar.png'}
     width={737}
     height={678}
     alt=''
