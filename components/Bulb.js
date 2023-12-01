711f75bd930d99ef942ae0bcf8954fec417e48ab
Youssafouhba <146470979+Youssafouhba@users.noreply.github.com>
2023-12-01 02:05:19 +0100

    Update Bulb.js

--- components/Bulb.js
+++ components/Bulb.js
@@ -7,7 +7,7 @@ const Bulb = () => {
   <div className='absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge
    animate-pulse duration-75 z-10 w-[200px] xl:w-[260px]'>
     <Image
-      src={'/bulb.png'}
+      src={'/public/bulb.png'}
       width={260}
       height={200}
       className='h-full w-full'
