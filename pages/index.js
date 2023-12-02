f646b3c4357e8e2d435c206e715c75b467987b33
Youssafouhba <146470979+Youssafouhba@users.noreply.github.com>
2023-12-02 02:25:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ const Home = () => {
     {/* image*/}
     <div className='w-[1200px] h-full absolute right-0 bottom-0'>
       {/**bg img */}
-      <div className=' bg-explosion xl xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
+      <div className='bg:none xl:bg-explosion xl xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
       </div>
       {/*particles*/}
       <ParticlesContainer />
