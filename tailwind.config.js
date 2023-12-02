76ec0b996de9be9649a93c5f40e95c258e1987b8
Youssafouhba <146470979+Youssafouhba@users.noreply.github.com>
2023-12-02 17:24:38 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ module.exports = {
         explosion: 'url("/bg-explosion.png")',
         circles: 'url("/bg-circles.png")',
         circleStar: 'url("/circle-star.svg")',
-        site: 'url(avatar)',
+        site: 'url({avatar})',
       },
       animation: {
         'spin-slow': 'spin 6s linear infinite',
