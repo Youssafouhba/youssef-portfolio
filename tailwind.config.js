48a21fe538ea148213efe6b94e00c2dd312a8f7c
Youssafouhba <146470979+Youssafouhba@users.noreply.github.com>
2023-12-02 23:59:37 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,10 +27,10 @@ module.exports = {
         accent: '#F13024',
       },
       backgroundImage: {
-        explosion: "url('/bg-explosio.png')",
-        circles: 'url("/bg-circles.png)',
-        circleStar: 'url("/circle-sta.svg")',
-        site: 'url("/site-b.svg")',
+        explosion: 'url("/bg-explosion.png")',
+        circles: 'url("/bg-circles.png")',
+        circleStar: 'url("/circle-star.svg")',
+        site: 'url("/site-bg.svg")',
       },
       animation: {
         'spin-slow': 'spin 6s linear infinite',
