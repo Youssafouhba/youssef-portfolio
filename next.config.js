9cf93d89a5b94e19454a2fe8b0f11989eaf93ac5
Youssafouhba <146470979+Youssafouhba@users.noreply.github.com>
2023-12-02 03:14:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-     experimental: {
-      externalDir: true
-    },
+
   basePath: "/youssef-portfolio",
   reactStrictMode: true,
   swcMinify: true,
    eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    //ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true,
   },
 };
 
