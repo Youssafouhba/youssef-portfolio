d52015c598b1002f6abd6abeeb94de3684cf40cb
Youssafouhba <146470979+Youssafouhba@users.noreply.github.com>
2023-12-02 02:56:05 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,9 @@ import React ,{useEffect,useState} from 'react';
 
 const Home = () => {
   return (
-  <div className="bg-primary/60  h-full">
+    <div className="bg-primary/60  h-full">
     {/*text*/}
-    <div className='w-full h-full bg-gradient-to-r
-     first-letter:from-primary/10 via-black/30 to-black/10'>
+    <div className='w-full h-full bg-gradient-to-r first-letter:from-primary/10 via-black/30 to-black/10'>
       <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
         {/*title */}
         <motion.h1 
@@ -59,7 +58,7 @@ const Home = () => {
     {/* image*/}
     <div className='w-[1200px] h-full absolute right-0 bottom-0'>
       {/**bg img */}
-      <div className=' xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
+      <div className='bg-none xl:bg-explosion xl xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'>
       </div>
       {/*particles*/}
       <ParticlesContainer />
@@ -70,8 +69,7 @@ const Home = () => {
         animate="show"
         exit="hidden"
         transition={{duration: 1,ease: "easeInOut"}}
-        className='w-full h-full max-w-[737px] 
-      max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]'>
+        className='w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]'>
         <Avatar />
       </motion.div>
     </div>
