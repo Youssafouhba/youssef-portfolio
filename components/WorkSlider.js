474f2bee26abe5a8025a916ec2eddf02d62973a9
Youssafouhba <146470979+Youssafouhba@users.noreply.github.com>
2023-12-03 17:23:24 +0100

    Update WorkSlider.js

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -94,7 +94,7 @@ const WorkSlider = () => {
             <div className='grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
               {slide.images.map((image,index) => {
                 return (
-                  <Link href={'image.link'}>
+                  <Link href={image.link}>
                 <div className='relative rounded-lg overflow-hidden flex items-center justify-center group'>
                   <div className='flex items-center justify-center relative overflow-hidden group'
                   key={index}>
