340a2fd6fb6bc326177a0f0d09c20cc4cff9c06e
Ouhba Youssef <146470979+Youssafouhba@users.noreply.github.com>
2024-01-17 00:11:41 +0100

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,26 +1,27 @@
 import {sendEmail} from './email';
 
+import { sendEmail } from './email';
+
 export default function handler(req, res) {
-    if (req.method === 'POST') {
-       const { name, email, message } = req.body;
+  if (req.method === 'POST') {
+    const { name, email, message } = req.body;
 
-        // Vérifier si toutes les données requises sont présentes
-        if (!name || !email || !message) {
-          return res.status(400).json({ message: 'Missing required fields' });
-        }
-
-      try {
-        // Appeler une fonction utilitaire pour envoyer l'email (par exemple, en utilisant SendGrid)
-        sendEmail(name, email, message);
-
-        // Répondre avec un statut 200 et un message de succès
-        return res.status(200).json({ message: 'Message sent successfully' });
-      } catch (error) {
-        console.error('Error sending email', error);
-        return res.status(500).json({ message: 'Error sending email' });
-      }
-   
-    }else {
-      res.status(405).json({ message: 'Method Not Allowed' });   
+    // Verify if all the required data is present
+    if (!name || !email || !message) {
+      return res.status(400).json({ message: 'Missing required fields' });
     }
+
+    try {
+      // Call a utility function to send the email (e.g., using SendGrid)
+      sendEmail(name, email, message);
+
+      // Respond with a 200 status and a success message
+      return res.status(200).json({ message: 'Message sent successfully' });
+    } catch (error) {
+      console.error('Error sending email', error);
+      return res.status(500).json({ message: 'Error sending email' });
+    }
+  } else {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
 }
