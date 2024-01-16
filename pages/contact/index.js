c35fbce1e75d8d5b3ba3e5645dc1542c3a142281
Ouhba Youssef <146470979+Youssafouhba@users.noreply.github.com>
2024-01-16 23:36:03 +0100

    Add files via upload

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,113 +1,25 @@
-import { useState } from 'react';
+import {sendEmail} from './email';
 
-import {Circles} from "/components/Circles";
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name, email, message } = req.body;
 
-import {BsArrowRight} from 'react-icons/bs';
-
-import { motion } from "framer-motion";
-
-import { fadeIn } from "../../variants";
-
-const Contact = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [status, setStatus] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    setStatus('Sending...');
-
-    // Envoyer le message côté serveur
-    try {
-      const response = await fetch('https://youssafouhba.github.io/youssef-portfolio/api', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, email,message }),
-      });
-      const data = await response.json();
-
-      if (response.ok) {
-        // Message envoyé avec succès
-        setStatus('Message sent successfully');
-        // Réinitialiser les champs du formulaire
-        setName('');
-        setEmail('');
-        setMessage('');
-      } else {
-        // Erreur lors de l'envoi du message
-        setStatus('Message sending failed&');
-      }
-    } catch (error) {
-        setStatus('Message sending failed');
+    // Vérifier si toutes les données requises sont présentes
+    if (!name || !email || !message) {
+      return res.status(400).json({ message: 'Missing required fields' });
     }
-  };
 
-  return (
-    <div className='h-full bg-primary/30'>
-      <div className='container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full'>
-        {/** text & form */}
-        <div className='flex flex-col w-full max-w-[700px]'>
-          {/** text */}
-          <motion.h2
-              variants={fadeIn('up', 0.2)}
-              initial='hidden'
-              animate= 'show'
-              exit='hidden'
-              className='h2 text-center mb-12'>
-            Let's <span className='text-accent'>connect</span>
-          </motion.h2>
-          {/** form */}
-          <motion.form
-              onSubmit={handleSubmit}
-              variants={fadeIn('up', 0.4)}
-              initial='hidden'
-              animate= 'show'
-              exit='hidden'
-              className='flex-1 flex flex-col gap-6 w-full mx-auto'>
-            {/** input group */}
-            <div className='flex gap-x-6 w-full'>
-              <input 
-              type="text" 
-              placeholder="name" 
-              value={name}
-              onChange={(e) => setName(e.target.value)} 
-              className='input'/>
-              <input 
-              type="email"
-              placeholder="email" 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className='input'/>
-            </div>
-            <textarea 
-            placeholder="message" 
-            className='textarea'
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          ></textarea>
-            <button
-            type='submit'
-            className='btn rounded-full border border-x-white/50 max-w-[170px] 
-            px-8 transition-all duration-300 flex items-center justify-center overflow-hidden
-            hover:border-accent group'>
-              <span className='group-hover:-translate-y-[120px] group-hover:opacity-0
-              transition-all duration-500'>Let's talk</span>
-              <BsArrowRight 
-              className='translate-y-[120px] opacity-0 group-hover:flex 
-              group-hover:-translate-y-0 group-hover:opacity-100 transition-all 
-              duration-300 absolute text-[22px]'/>
-            </button>
-            {status && <p>{status}</p>}
-          </motion.form>
-        </div>
-      </div>
+    try {
+      // Appeler une fonction utilitaire pour envoyer l'email (par exemple, en utilisant SendGrid)
+      sendEmail(name, email, message);
 
-    </div>
-    );
-};
-
-export default Contact;
+      // Répondre avec un statut 200 et un message de succès
+      return res.status(200).json({ message: 'Message sent successfully' });
+    } catch (error) {
+      console.error('Error sending email', error);
+      return res.status(500).json({ message: 'Error sending email' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
