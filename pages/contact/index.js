2d480b818e62337520e556ad95ad3419517ae86d
Ouhba Youssef <146470979+Youssafouhba@users.noreply.github.com>
2024-01-16 23:42:48 +0100

    Update index.js

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -21,7 +21,7 @@ const Contact = () => {
 
     // Envoyer le message côté serveur
     try {
-      const response = await fetch('/contact', {
+      const response = await fetch('./contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
