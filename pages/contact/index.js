8284627f28dcc8496ba3d2736c5a8e34f4ce4fac
Ouhba Youssef <146470979+Youssafouhba@users.noreply.github.com>
2024-01-16 19:51:01 +0100

    Update index.js

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -21,7 +21,7 @@ const Contact = () => {
 
     // Envoyer le message côté serveur
     try {
-      const response = await fetch('https://github.com/Youssafouhba/youssef-portfolio/api/index.js', {
+      const response = await fetch('/api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
