88d605804109de7daafa630b6b3dd652bdde6186
Ouhba Youssef <146470979+Youssafouhba@users.noreply.github.com>
2024-01-17 01:38:53 +0100

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,28 @@
-import {sendEmail} from '../../api/email';
+import nodemailer from 'nodemailer';
+
+
+// Create a transporter for sending emails
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com', // Replace with your SMTP server host
+  port: 587, // Replace with the SMTP server port
+  secure: false, // Set to true if using a secure connection (e.g., SSL/TLS)
+  auth: {
+    user: 'youssefouhba@gmail.com', // Replace with your email address
+    pass: 'wtje huhv azdm dsjo', // Replace with your email password or an app-specific password
+  },
+});
+
+export async function sendEmail(name, email, message) {
+  const mailOptions = {
+    from: 'youssafouhba@gmail.com',
+    to: 'youssefouhba@gmail.com',
+    subject: 'New Message from Contact Form',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  // Send the email using the transporter
+  await transporter.sendMail(mailOptions);
+}
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
