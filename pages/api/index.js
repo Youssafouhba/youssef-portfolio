6f13a0e25c4bdb0d9069ddf8909031254ea480dd
Ouhba Youssef <146470979+Youssafouhba@users.noreply.github.com>
2024-01-18 05:50:45 +0100

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,3 +1,35 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+import {sendEmail} from './email';
+
+const handler = async (req, res) => {
+  try {
+    const { method } = req;
+    switch (method) {
+      case "POST": {
+        //Do some thing
+        await sendEmail(
+          "TEST",
+          "dontkillme@bunnyfiedlabs.com",
+          "THI IS A TEST FOR MY MEDIUM USERS"
+        );
+        res.status(200).send("Success");
+        break;
+      }
+      case "GET": {
+        //Do some thing
+        res.status(200).send(req.auth_data);
+        break;
+      }
+      default:
+        res.setHeader("Allow", ["POST", "GET", "PUT", "DELETE"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+        break;
+    }
+  } catch (err) {
+    res.status(400).json({
+      error_code: "api_one",
+      message: err.message,
+    });
+  }
+};
+
+export default handler;
